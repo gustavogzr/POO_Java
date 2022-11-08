42beaf9ba10a898a0440bd9f013f238ce5b9a688
gustavogzr <gustavogzr@hotmail.com>
2022-11-07 23:30:26 -0300

    herança + polimorfismo

---
+++ Aluno.java
@@ -0,0 +1,8 @@
+import java.sql.Date;
+
+public class Aluno extends Pessoa {
+    public Aluno(String _nome, String _cpf, Date _data) {
+        super(_nome,_cpf,_data);
+    }
+    public String matricula;
+}
---
+++ Professor.java
@@ -0,0 +1,9 @@
+import java.sql.Date;
+
+public class Professor extends Pessoa {
+    public Professor(String _nome, String _cpf, Date _data) {
+        super(_nome,_cpf,_data);
+    }
+    public double salario;
+    public String disciplina;
+}
